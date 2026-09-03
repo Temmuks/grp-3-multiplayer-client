4870a59b0141eba745fb560f96335ae85d805558
williameliasson <williameliasson@users.noreply.github.com>
2026-09-03 11:22:18 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <></>;
+  return <>
+    <h1>App</h1>
+  </>;
 }
 
 export default App;
