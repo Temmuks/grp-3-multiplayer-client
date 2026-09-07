664b1eda709a17ebcfe08ac8249288f66abf94d3
williameliasson <williameliasson@users.noreply.github.com>
2026-09-07 18:31:45 +0200

    subscribed to /topic/game/{gameRoomId} upon mount of GamePage

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,7 +1,27 @@
 import { useParams } from "react-router"
+import { useWebSocket } from "../contexts/WebSocketContext";
+import { useEffect } from "react";
+import type { IMessage } from "@stomp/stompjs";
 
 function GamePage() {
     const { gameRoomId } = useParams();
+    const client = useWebSocket();
+
+    function handleGameRoomUpdate(message: IMessage){
+        console.log("got message")
+    }
+
+    useEffect(() => {
+        if (!client){
+            return;
+        }
+
+        const subscription = client.subscribe(`/topic/game/${gameRoomId}`, handleGameRoomUpdate)
+
+        return () => {
+            subscription.unsubscribe()
+        }
+    }, [client])
   return (
     <div>
         <h1>Game Page</h1>
