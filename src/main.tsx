fe462539feccb609b0db33973585d6bdc9fa5bbe
WWolfburg <loke.wik@gmail.com>
2026-09-13 12:53:22 +0200

    maybe good? maybe not?

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { WebSocketProvider } from './contexts/WebSocketContext.tsx'
+import './App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
