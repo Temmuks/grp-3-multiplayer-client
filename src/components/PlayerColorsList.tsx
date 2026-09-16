3e993f5693c087b3e6091850ee12da2f9543876c
williameliasson <williameliasson@users.noreply.github.com>
2026-09-16 20:47:24 +0200

    comments and styling

--- src/components/PlayerColorsList.tsx
+++ src/components/PlayerColorsList.tsx
@@ -6,13 +6,17 @@ type PlayerColorsListProps = {
 }
 
 function PlayerColorsList({maxPlayers, colors} : PlayerColorsListProps) {
-    const UNOCCUPIED_SPOT_COLOR = "darkgray";
+    const UNOCCUPIED_SPOT_COLOR = "#202023";
     const circles: React.ReactNode[] = [];
+
+    // Add circles for each joined player's color
     colors.forEach((color) => {
         circles.push(
             <CircleComponent key={"color-" + color} color={color}/>
         )
     })
+
+    // Add unoccupied player slots represented by some color 'UNOCCUPIED_SPOT_COLOR'
     for (let i = colors.length; i<maxPlayers; i++){
         circles.push(
             <CircleComponent key={"color-" + i} color={UNOCCUPIED_SPOT_COLOR} />
